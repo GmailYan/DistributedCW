74d45b26ff7203b06855dff497c7c6a213bc369d
GmailYan <yanbzybzy2@gmail.com>
2013-02-27 10:41:01 +0000

    file rename for makefile to work properly

--- src/Broadcaster.java
+++ src/Broadcaster.java
@@ -1,3 +1,4 @@
+package src;
 import java.lang.Math;
 
 class Broadcaster extends Process {
---
+++ src/FailureDetector.java
@@ -0,0 +1,37 @@
+package src;
+
+
+
+public class FailureDetector implements IFailureDetector {
+
+	@Override
+	public void begin() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void receive(Message m) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean isSuspect(Integer process) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int getLeader() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void isSuspected(Integer process) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
