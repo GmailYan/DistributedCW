5a232ffbc11044f08f5df56334f6f8f5b2241b9d
GmailYan <yanbzybzy2@gmail.com>
2013-03-12 18:46:51 +0000

    shitloads of code done, while shitloads of debug to do

--- ESFDProcess.java
+++ ESFDProcess.java
@@ -1,30 +1,31 @@
 
-public class SFDProcess extends Process {
+public class ESFDProcess extends Process {
 
-	int pid;
 	private int x;
-	public SFDProcess(String name, int pid, int n, int x) {
-		super(name, pid, n);
-		this.pid = pid;
+
+	public ESFDProcess(String name, int id, int size, int x) {
+		super(name, id, size);
 		this.setX(x);
-		detector = new StrongFailureDetector(this);
+		detector = new EventuallyStrongFailureDetector(this);
 	}
 
-	private static StrongFailureDetector detector;
-	
+	private EventuallyStrongFailureDetector detector;
+
 	public void begin() {
 		detector.begin();
 	}
-	
+
 	public synchronized void receive(Message m) {
 		String type = m.getType();
 		if (type.equals("heartbeat")) {
 			detector.receive(m);
-		} else if (type.equals("VAL") ) {
-			detector.m.put(m.getSource(),m);
+		} else if(type.equals("VAL")){
+			detector.addConsensusMessage(m);
+		} else if(type.equals("OUTCOME")){
+			detector.addOutcomeMessage(m);
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		String name = args[0];
 		int id = Integer.parseInt(args[1]);
@@ -32,10 +33,12 @@ public class SFDProcess extends Process {
 		int x = Integer.parseInt(args[3]);
 		// for consensus: read x implies that there is an extra command line argument for each process
 		
-		SFDProcess p = new SFDProcess(name, id, n, x);
+		ESFDProcess p = new ESFDProcess(name, id, n, x);
 		p.registeR();
 		p.begin();
 		p.Consensus();
+
+		
 	}
 
 	private void Consensus() {
@@ -43,12 +46,12 @@ public class SFDProcess extends Process {
 		Utils.out("End of consensus, "+x+" is decided");
 	}
 
-	int getX() {
+	public int getX() {
 		return x;
 	}
 
-	void setX(int x) {
+	public void setX(int x) {
 		this.x = x;
 	}
-
+	
 }
